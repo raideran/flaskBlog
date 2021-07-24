8edad6561a45cabbb17c2884e365cef69922a261
Jose Porras <japslatino@gmail.com>
2021-07-24 01:47:57 -0600

    Adding confirmation functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,19 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
+from .forms import LoginForm, RegistrationForm
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from ..email import send_email
 
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,10 +31,56 @@ def login():
         flash('Invalid email or password.')
     return render_template('auth/login.html', form=form)
 
-
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, '[Flasky Blog] Confirmation Email', 'auth/email/confirm',
+                   user=user, token=token)                    
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm_token(token):
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!')
+    return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, '[Flasky Blog] Confirm Your Account',
+                'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from . import login_manager
+from . import db
 
 class Role(db.Model):
     __tablename__ = 'role'
@@ -19,6 +21,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default = False)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
     def __repr__(self):
@@ -35,6 +38,24 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def generate_confirmation_token(self, expiration=3600):
+        serial = Serializer(current_app.config['SECRET_KEY'], expiration)
+        
+        return serial.dumps({'confirm': self.id})        
+
+    def confirm_token(self, token):
+        serial = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = serial.loads(token)            
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
+        
 
 @login_manager.user_loader
 def load_user(user_id):
